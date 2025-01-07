52199065d82ab5ba179e11ae61b73f187b931557
vishalsirpasaalai <narasimhavishal@gmail.com>
2025-01-07 19:05:27 +0530

    Update headerFooterManager.js
    Added the Google Search Console

--- website/headerFooterManager.js
+++ website/headerFooterManager.js
@@ -2,15 +2,16 @@ class SpecialHead extends HTMLElement {
     connectedCallback(){
         this.innerHTML= `
         <head>
+        <meta name="google-site-verification" content="wb24FXhCdpGpT-cHcnwNcE_DECo9UE7-8TYQ35vGRk4" />
         <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-78BDNVY2HK"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-78BDNVY2HK');
-</script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-78BDNVY2HK"></script>
+      <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-78BDNVY2HK');
+      </script>
         
       <!-- basic -->
       <meta charset="utf-8">
